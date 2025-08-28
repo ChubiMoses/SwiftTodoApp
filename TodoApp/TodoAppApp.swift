8fcb9a297f664e52abbb5f2d8051b378253a57cb
ChubiMoses <chubiguit@gmail.com>
2025-08-28 10:52:32 +0100

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by MAC on 8/28/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
